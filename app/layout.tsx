d6377691406903e1cdb790d9a1bec9f5b1d14d97
abdulloyev <abdulloyev.uz@gmail.com>
2025-03-31 18:09:04 +0500

    #creat - NewProject nextjs-15.2.4, first commit

---
+++ app/layout.tsx
@@ -0,0 +1,41 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono, Kanit } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+const kanit = Kanit({
+  variable: "--font-kanit",
+  subsets: ["latin"],
+  weight: ["600"],
+});
+
+export const metadata: Metadata = {
+  title: "Focus Session | LensFocus",
+  description:
+    "Product by ABUcoder Team. Maximize your focus and efficiency with LensFocus. Work smarter, achieve more.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} ${kanit.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="flex flex-col justify-center items-center h-screen">
+      <h1 className="text-4xl font-mono">LensFocus :)</h1>
+
+      <p className="text-8xl font-kanit">25:00</p>
+    </div>
+  );
+}
