10be99ad01f55f555ae287269eba93a8985edfe8
abdulloyev <abdulloyev.uz@gmail.com>
2025-04-01 13:00:58 +0500

    #add - constants file, timer modes

---
+++ lib/constants.ts
@@ -0,0 +1,21 @@
+// Timer modes
+export const TIMER_MODES = {
+  FOCUS: {
+    id: "focus",
+    name: "Focus",
+    minutes: 25,
+    seconds: 0,
+  },
+  SHORT_BREAK: {
+    id: "shortBreak",
+    name: "Short Break",
+    minutes: 0,
+    seconds: 10,
+  },
+  LONG_BREAK: {
+    id: "longBreak",
+    name: "Long Break",
+    minutes: 15,
+    seconds: 0,
+  },
+};
